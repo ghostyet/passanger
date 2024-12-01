b69dda65e49c5a51f93df4f8b4de647d8c3174c1
Ezra Kimurgor <125592182+ghostyet@users.noreply.github.com>
2024-12-01 14:23:22 -0800

    small changes

--- index.js
+++ index.js
@@ -13,3 +13,40 @@ gameStarts.addEventListener (  "click", function(){
         stageEl.textContent=group1+" Vs"+group2
         
     })
+
+    const fruits=["🍎","🍎","🍎","🍎","🍊","🍊","🍊","🍊","🍊","🍑","🍑","🍑","🍑",
+        "🍌","🍌","🍌","🍌","","","","","",""]
+
+
+
+let appleShelf=document.getElementById("apple=shelf")
+let orangeShelf=document.getElementById("orange-shelf")
+let bananaShelf=document.getElementById("banana-shelf")
+
+
+    function fruitSort(){
+        for(let fruits=i;i<fruits.length;i++){
+            if(fruits[i]==="🍎"){
+                appleShelf.textContent+="🍎"
+            }
+            else if(fruits[i]==="🍊"){
+                orangeShelf.textContent+="🍊"
+            }
+            else if (fruits[i]===""){
+                mangoShelf.textContent+=""
+            }
+else if(fruits[i]==="🍑"){
+   burgerShelf.textContent+="🍑"
+}
+
+else if(fruits[i]==="🍌"){
+    bananaShelf.textContent🍌
+
+
+
+
+        }
+        
+    }
+    }
+    fruitSort()
